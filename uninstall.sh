413ee865fa43520e8545da17a630e8e2a97d2d8f
Nickolas Means <nick@symops.io>
2024-01-26 11:49:43 -0600

    Initial post-fork updates

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ipath=/usr/local/share/dump1090-retro-html
+ipath=/usr/local/share/dump1090-retro-ipad
 
 rm -rf $ipath
 
-rm /etc/lighttpd/conf-enabled/88-dump1090-retro-html.conf
-rm /etc/lighttpd/conf-available/88-dump1090-retro-html.conf
+rm /etc/lighttpd/conf-enabled/88-dump1090-retro-ipad.conf
+rm /etc/lighttpd/conf-available/88-dump1090-retro-ipad.conf
 
 systemctl restart lighttpd
 
 echo --------------
 echo --------------
-echo "dump1090-retro-html: Uninstall complete!"
+echo "dump1090-retro-ipad: Uninstall complete!"
